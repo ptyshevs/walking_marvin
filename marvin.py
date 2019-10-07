77e06d459caa5c510f9debbcdb6e1a517b7840e1
Pavel Tyshevskyi <ptyshevs@gmail.com>
2019-10-07 11:53:51 +0300

    Moved notebooks to directory, merged Livii backprop

--- marvin.py
+++ marvin.py
@@ -9,62 +9,113 @@ from ESRaySolver import *
 
 from viz import render_env
 
+def train_backprop(nn, env):
+	for i_episode in range(50):
+		losses = []
+		observation_arr = []
+		cum_reward = 0
+		done = False
+
+		print("Epoch #", i_episode)
+
+		observation = env.reset()
+		while not done:
+			env.render()
+
+			observation = np.array(observation).reshape((24,))
+			observation_arr.append(observation)
+
+			action = nn.predict(observation).reshape((4))
+			observation, reward, done, info = env.step(action)
+
+			cum_reward = reward - cum_reward
+			nn.nn_backprop_one(observation, cum_reward)
+
+			losses.append(cum_reward)
+
+			if done:
+				# print("Episode finished after {} timesteps".format(t+1))
+				break
+
+def render_backprop(nn, env):
+	done = False
+	observation = env.reset()
+
+	while not done:
+		env.render()
+		observation = np.array(observation).reshape((24,1))
+		observation_arr.append(observation)
+
+		action = nn.nn_prop(observation).reshape((4))
+		observation, reward, done, info = env.step(action)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--walk', '-w', default=False, help="Display only walking process", action='store_true')
-    parser.add_argument('--load', '-l', help="Load weights for Marvin agent from a file, skipping training process")
-    parser.add_argument('--save', '-s', help="Save weights to a file after running the program")
-    parser.add_argument('--multiprocess', '-m', help="Spread training across multiple processes", default=False, action='store_true')
-    parser.add_argument('--detailed-log', '-d', default=False, help="Display detailed log", action='store_true')
-    parser.add_argument('--sonic', default=False, help="--walk, but swiftly", action='store_true')
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--walk', '-w', default=False, help="Display only walking process", action='store_true')
+	parser.add_argument('--load', '-l', help="Load weights for Marvin agent from a file, skipping training process")
+	parser.add_argument('--save', '-s', help="Save weights to a file after running the program")
+	parser.add_argument('--multiprocess', '-m', help="Spread training across multiple processes", default=False, action='store_true')
+	parser.add_argument('--detailed-log', '-d', default=False, help="Display detailed log", action='store_true')
+	parser.add_argument('--sonic', default=False, help="--walk, but swiftly", action='store_true')
+	parser.add_argument('--backprop', default=False, help="Use Backprop-based training", action='store_true')
+	args = parser.parse_args()
 
-    layer_sizes = [24, 24, 4]
-    agent = NN(layer_sizes, seed=0)
+	layer_sizes = [24, 24, 4]
 
-    env_name = "Marvin-v0"
-    env = gym.make(env_name)
+	if args.backprop:
+		agent = NNBackProp()
+		agent.init_nn()
+	else:
+		agent = NN(layer_sizes, seed=0)
 
-    train_phase = True
-    if args.load is not None:
-        try:
-            with open(args.load, 'rb') as f:
-                weights = pcl.load(f)
-                agent.set_weights(weights)
-                train_phase = False
-        except FileNotFoundError:
-            print(f"Failed to load file: {args.load}")
-            sys.exit(1)
-    if args.sonic:
-        with open("fast_marvin.pcl", 'rb') as f:
-            weights = pcl.load(f)
-            agent.set_weights(weights)
-            train_phase = False
+	env_name = "Marvin-v0"
+	env = gym.make(env_name)
 
-    if args.walk:
-        train_phase = False
+	train_phase = True
+	if args.load is not None:
+		if args.backprop:
+			agent.load_weights(args.load)
+		else:
+			try:
+				with open(args.load, 'rb') as f:
+					weights = pcl.load(f)
+					agent.set_weights(weights)
+					train_phase = False
+			except FileNotFoundError:
+				print(f"Failed to load file: {args.load}")
+				sys.exit(1)
+	if args.sonic and not args.backprop:
+		with open("fast_marvin.pcl", 'rb') as f:
+			weights = pcl.load(f)
+			agent.set_weights(weights)
+			train_phase = False
 
+	if args.walk:
+		train_phase = False
 
-    if train_phase:
-        # Train the agent
-        if args.multiprocess:
-            solver = ESRaySolver(agent, env_name, verbose=True)
-        else:
-            solver = ESSolver(agent, env_name, verbose=True)
-        
-        optimal_weights = solver.solve(detailed_log=args.detailed_log)
-        agent.set_weights(optimal_weights)
+	# Train the agent
+	if train_phase:
+		if args.backprop:
+			train_backprop(agent, env)
+		else:
+			if args.multiprocess:
+				solver = ESRaySolver(agent, env_name, verbose=True)
+			else:
+				solver = ESSolver(agent, env_name, verbose=True)
+		
+			optimal_weights = solver.solve(detailed_log=args.detailed_log)
+			agent.set_weights(optimal_weights)
 
-    if args.save:
-        with open(args.save, 'wb') as f:
-            pcl.dump(agent.get_weights(), f)
-    
-    render_env(agent, env)
-    
-    
-
-
-    
-
-
-
+	if args.save:
+		if args.backprop:
+			agent.save_weights(args.save)
+		else:
+			with open(args.save, 'wb') as f:
+				pcl.dump(agent.get_weights(), f)
+	
+	if args.backprop:
+		render_backprop(agent, env)
+	else:
+		render_env(agent, env)
+	env.close()
--- nn.py
+++ nn.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pickle
+
 
 class NN:
     def __init__(self, layer_sizes, seed=None):
@@ -27,3 +29,122 @@ class NN:
         if copy:
             return [np.copy(l) for l in self.weights]
         return self.weights
+
+
+class NNBackProp:
+    def __init__(self):
+        np.random.seed(12334)
+
+        self.input_size = 24
+        self.learning_rate = 0.000001
+
+        self.layers_size = [self.input_size, 32, 4]
+        self.nn = []
+        self.bias = []
+
+        self.activations = []
+        self.derivatives = []
+
+    def tanh(self, X):
+        return (np.exp(X) - np.exp(X * -1)) / (np.exp(X) + np.exp(X * -1))
+
+    def tanh_derivative(self, X):
+        return 1 - (self.tanh(X) ** 2)
+
+    def relu(self, X):
+        return np.maximum(X, 0)
+
+    def relu_derivative(self, X):
+        X[X <= 0] = 0
+        X[X > 0] = 1
+
+        return X
+
+    def init_nn(self):
+        self.activations = [self.relu, self.tanh]
+        self.derivatives = [self.relu_derivative, self.tanh_derivative]
+
+        for i in range(len(self.layers_size)-1):
+            self.nn.append(np.random.randn(self.layers_size[i], self.layers_size[i+1]))
+            self.bias.append(np.random.randn(self.layers_size[i+1], 1))
+
+    def get_nn_prop_history(self, X):
+        prev_dim = 1 if len(X.shape) < 2 else X.shape[1]
+        prev = X.reshape( (X.shape[0], prev_dim) )
+        history = []
+
+        for i in range(len(self.nn)):
+            curr_layer = np.transpose(self.nn[i])
+            curr_bias = self.bias[i]
+
+            current_state = self.activations[i](curr_layer.dot(prev) + curr_bias)
+            prev = current_state
+            history.append(current_state)
+
+        return history
+
+    def nn_backprop_one(self, observation, cum_reward):
+        observation = np.array(observation).reshape((24,))
+        history = self.get_nn_prop_history(observation)
+
+        losses = np.transpose(np.array([cum_reward] * 4))
+        o_0 = losses * self.derivatives[-1](history[-2].T.dot(self.nn[-1]) + self.bias[-1].reshape((self.bias[-1].shape[0],)))
+        o_0 = o_0[0]
+
+        o_1 = []
+
+        for i in range(self.layers_size[-2]):
+            o_1_tmp = 0
+            for j in range(self.layers_size[-1]):
+                delta = self.learning_rate * o_0[j] * history[-2][i]
+                o_1_tmp += self.nn[-1][i][j] * o_0[j]
+                self.nn[-1][i][j] += delta
+
+            o_1.append(o_1_tmp)
+
+        o_1 = np.array(o_1)
+        o_1 = o_1 * self.derivatives[-1](observation.T.dot(self.nn[-2]) + self.bias[-2].reshape((self.bias[-2].shape[0],)))
+
+        for i in range(self.layers_size[-3]):
+            for j in range(self.layers_size[-2]):
+                delta = self.learning_rate * o_1[j] * observation[i]
+                self.nn[-2][i][j] += delta
+
+    def nn_backprop(self, observations, rewards):
+        cum_rewards = []
+        cum_rew = 0
+
+        for reward in rewards:
+            cum_rew = reward - cum_rew
+            cum_rewards.append(cum_rew)
+
+        for i in range(len(observations)):
+            self.nn_backprop_one(observations[i], cum_rewards[i])
+
+    def predict(self, X):
+        prev_dim = 1 if len(X.shape) < 2 else X.shape[1]
+        prev = X.reshape( (X.shape[0], prev_dim) )
+
+        for i in range(len(self.nn)):
+            curr_layer = np.transpose(self.nn[i])
+            curr_bias = self.bias[i]
+
+            current_state = self.activations[i](curr_layer.dot(prev) + curr_bias)
+            prev = current_state
+
+        return prev
+
+    def save_weights(self, file):
+        model = [self.nn, self.bias]
+        pickle.dump(model, open(file, 'wb'))
+
+    def load_weights(self, file):
+        try:
+            model = pickle.load(open(file, 'rb'))
+        except FileNotFoundError:
+            print("File not found:", file)
+            exit(1)
+        self.nn = model[0]
+        self.bias = model[1]
+
+
